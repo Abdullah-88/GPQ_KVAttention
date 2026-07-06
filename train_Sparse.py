63ddd3390feb67aa5b3a591db0caf88d3645763e
Abdullah Nazhat Abdullah <abdullah.nazhat@gmail.com>
2026-07-07 02:20:20 +0300

    Update train_Sparse.py

--- train_Sparse.py
+++ train_Sparse.py
@@ -7,8 +7,6 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, Normalize, Compose
 from GPQ_KVAttention_Sparse import SGPQ_KVAttentionTransformer
 
-
-
 transform = Compose([
  
 ToTensor(),
@@ -30,27 +28,19 @@ test_data = datasets.CIFAR10(
                                        transform=transform
                                        )
 
-
 batch_size = 128
 
 train_dataloader = DataLoader(training_data, batch_size=batch_size,shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
 
 for X, y in test_dataloader:
     print(f"Shape of X [N,C,H,W]:{X.shape}")
     print(f"Shape of y:{y.shape}{y.dtype}")
     break
 
-
-
-
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print(f"using {device} device")
-
-
 
 class TransformerImageClassification(SGPQ_KVAttentionTransformer):
     def __init__(
@@ -62,7 +52,6 @@ class TransformerImageClassification(SGPQ_KVAttentionTransformer):
         d_model = 256,
         num_heads = 8,
         num_layers=4,
-
 
     ):
         super().__init__(d_model, num_heads, num_layers)
@@ -85,13 +74,8 @@ class TransformerImageClassification(SGPQ_KVAttentionTransformer):
 model = TransformerImageClassification().to(device)
 print(model)
 
-
-
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
-
-
-
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -102,10 +86,8 @@ def train(dataloader, model, loss_fn, optimizer):
     for batch, (X,y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
 
-
         pred = model(X)
         loss = loss_fn(pred,y)
-
 
         optimizer.zero_grad()
         loss.backward()
@@ -113,9 +95,6 @@ def train(dataloader, model, loss_fn, optimizer):
         train_loss += loss.item()
         _, labels = torch.max(pred.data, 1)
         correct += labels.eq(y.data).type(torch.float).sum()
-
-
-
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
@@ -125,10 +104,6 @@ def train(dataloader, model, loss_fn, optimizer):
     train_accuracy = 100. * correct.item() / size
     print(train_accuracy)
     return train_loss,train_accuracy
-
-
-
-
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -148,17 +123,12 @@ def test(dataloader, model, loss_fn):
     test_accuracy = 100*correct
     return test_loss, test_accuracy
 
-
-
-
-
 logname = "/PATH/Experiments_cifar10/logs_sgpq_kva/logs_cifar10.csv"
 if not os.path.exists(logname):
   with open(logname, 'w') as logfile:
     logwriter = csv.writer(logfile, delimiter=',')
     logwriter.writerow(['epoch', 'train loss', 'train acc',
                         'test loss', 'test acc'])
-
 
 epochs = 100
 for epoch in range(epochs):
@@ -169,9 +139,8 @@ for epoch in range(epochs):
         logwriter = csv.writer(logfile, delimiter=',')
         logwriter.writerow([epoch+1, train_loss, train_acc,
                             test_loss, test_acc])
+        
 print("Done!")
-
-
 
 path = "/PATH/Experiments_cifar10/weights_sgpq_kva"
 model_name = "GPQ-KVAImageClassification_cifar10"
